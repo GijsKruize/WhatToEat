a21b97893a007a439ad057287d7856b670833143
Gijs Kruize <gbmkruize@gmail.com>
2023-03-16 15:26:39 +0100

    cleaned up some stuff

---
+++ app/src/main/java/com/example/whattoeat/ui/food/SwipeListener.java
@@ -0,0 +1,65 @@
+package com.example.whattoeat.ui.food;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.yalantis.library.KolodaListener;
+
+public class SwipeListener implements KolodaListener {
+    private Context context;
+
+    public SwipeListener(Context context){
+        this.context = context;
+    }
+    @Override
+    public void onCardDoubleTap(int i) {
+
+    }
+
+    @Override
+    public void onCardDrag(int i, @NonNull View view, float v) {
+
+    }
+
+    @Override
+    public void onCardLongPress(int i) {
+
+    }
+
+    @Override
+    public void onCardSingleTap(int i) {
+    }
+
+    @Override
+    public void onCardSwipedLeft(int i) {
+        Toast.makeText(context, "Disliked recipe", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCardSwipedRight(int i) {
+        Toast.makeText(context, "Liked recipe", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClickLeft(int i) {
+
+    }
+
+    @Override
+    public void onClickRight(int i) {
+
+    }
+
+    @Override
+    public void onEmptyDeck() {
+        Toast.makeText(context, "That was all for today!", Toast.LENGTH_SHORT).show();;
+    }
+
+    @Override
+    public void onNewTopCard(int i) {
+
+    }
+}
