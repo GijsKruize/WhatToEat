f88493c730b231c38f651b42b8ddfd1cd31585f4
Loekstars <loekstars@hotmail.com>
2023-04-03 19:01:21 +0200

    Small changes to the homepage

--- app/src/main/java/com/example/whattoeat/ui/home/homepage.java
+++ app/src/main/java/com/example/whattoeat/ui/home/homepage.java
@@ -58,25 +58,33 @@ public class homepage extends Fragment {
 //    HashMap<String, Integer> cuisines;
 //    public homepageFilters filter = new homepageFilters();
 
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_homepage, container, false);
 
+        // Initialize the views
         mListView = view.findViewById(R.id.listview);
         progressBar = view.findViewById(R.id.progressBarHome);
 
         MyAdapter adapter = new MyAdapter();
         mListView.setAdapter(adapter);
         try {
+            // Get the user's preferences
             getPreference();
+            // Get the styles that match the user's mood
             prefStyles = getStyles(prefMood);
         } catch (Exception e){
             Log.e("Exception Homepage: ", "Data was empty in onCreate");
         }
         fetchData(); // Load the data
+
         return view;
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,12 +100,18 @@ public class homepage extends Fragment {
         }
     }
 
+    /**
+     * Called when the fragment is visible to the user and actively running.
+     * Resumes fetching the data.
+     */
     @Override
     public void onResume() {
         super.onResume();
         isDataShuffled = false; // Reset the variable to false
         try{
+            // Get the user's preferences
             getPreference();
+            // Get the styles that match the user's mood
             prefStyles = getStyles(prefMood);
 
         }catch (Exception e){
@@ -128,28 +142,35 @@ public class homepage extends Fragment {
      * If the user has not set any preference, the default values will be used.
      */
     private void getPreference(){
+        // Get a reference to the "Preference" node in the database
         DatabaseReference preferences = database.getReference("Preference");
+
+        // Get the currently signed-in user
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Get the user's unique ID
         final String UID = user.getUid();
 
+        // Retrieve the user's preference data from the database
         preferences.child(UID).get()
                 .addOnCompleteListener(task -> {
                     String mood = "Happy";
                     String location = "both";
                     try {
+                        // Get the user's mood and location preferences, if available
                         mood = task.getResult().child("Mood").getValue().toString();
                         location = task.getResult().child("Location").getValue().toString();
                     } catch (NullPointerException e) {
-                        Log.d("Homepage: ",
-                                "No location or mood data found for user. " +
-                                        "User must set this in preferences page! " +
-                                        "Default values chosen.");
+                        // If there is no mood or location preference data for the user, use default values
+                        Log.d("Homepage: ", "No location or mood data found for user. " +
+                                "User must set this in preferences page! " +
+                                "Default values chosen.");
                     }
+                    // Set the user's preferences in the app
                     setPreference(mood, location);
                 }).addOnFailureListener(task2 ->{
-                    Log.e("Homepage: ",
-                            "Error getting data from database! " +  task2);
-
+                    // Log an error message if there is a problem retrieving the data from the database
+                    Log.e("Homepage: ", "Error getting data from database! " +  task2);
                 });
     }
 
@@ -178,13 +199,18 @@ public class homepage extends Fragment {
         myRefRecipe.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Clear the lists that hold recipe data
                 listIdsRec.clear();
                 listNamesRec.clear();
                 listImagesRec.clear();
                 listStyleRec.clear();
+
+                // Check if the user's location preference is "out"
+                // If it is, don't include recipes in the list
                 if(pref == null || !pref.equals("out")) {
+                    // Loop through all the recipes in the database
                     for (DataSnapshot recipeSnapshot : dataSnapshot.getChildren()) {
-                        // retrieve data for each recipe
+                        // Retrieve data for each recipe
 //                        Log.e("Homepage", finalStyles.toString() + recipeSnapshot.child("Style").getValue(String.class));
                         if(finalStyles.contains(recipeSnapshot.child("Style").getValue(String.class))) {
 
@@ -193,6 +219,7 @@ public class homepage extends Fragment {
                             String recipeImage = recipeSnapshot.child("Image").getValue(String.class);
                             String style = recipeSnapshot.child("Style").getValue(String.class);
 
+                            // Add the recipe data to the appropriate lists
                             listIdsRec.add(recipeId);
                             listNamesRec.add(recipeName);
                             listImagesRec.add(recipeImage);
@@ -216,10 +243,15 @@ public class homepage extends Fragment {
         myRefRestaurant.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot1) {
+                // Clear the lists that hold restaurant data
                 listIdsRest.clear();
                 listNamesRest.clear();
                 listImagesRest.clear();
+
+                // Check if the user's location preference is "home"
+                // If it is, don't include restaurants in the list
                 if(pref == null || !pref.equals("home")) {
+                    // Loop through all the restaurants in the database
                     for(DataSnapshot Restaurant : dataSnapshot1.getChildren()) {
                         // retrieve data for each recipe
                         if(finalStyles.contains(Restaurant.child("Style").getValue(String.class))) {
@@ -228,6 +260,7 @@ public class homepage extends Fragment {
                             String restaurantImage = Restaurant.child("Image").getValue(String.class);
                             Boolean verified = Restaurant.child("Verified").getValue(Boolean.class);
 
+                            // Only add the restaurant data to the list if it is verified
                             if (verified) {
                                 listIdsRest.add(restaurantId);
                                 listNamesRest.add(restaurantName);
@@ -249,6 +282,11 @@ public class homepage extends Fragment {
         });
     }
 
+    /**
+     * This method is used to get the styles from the database that are bound to the user's mood.
+     * @param mood The user's mood
+     * @return List of styles bound to mood
+     */
     private List<String> getStyles(String mood){
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("General Relation");
         List<String> styles = new ArrayList<>();
@@ -256,13 +294,12 @@ public class homepage extends Fragment {
             for(DataSnapshot data: result.getResult().getChildren()){
                 Integer value = Integer.parseInt(data.getValue().toString());
                 if (value >= 0) {
-                    styles.add(data.getKey());
+                    String style = data.getKey();
+                    styles.add(style);
                 }
             }
             prefStyles = styles;
             fetchFromDb(styles, prefLocation);
-
-        }).addOnFailureListener(failed ->{
 
         });
         return styles;
@@ -289,18 +326,19 @@ public class homepage extends Fragment {
         public View getView(int i, View view, ViewGroup viewGroup) {
             view = getLayoutInflater().inflate(R.layout.card, viewGroup, false);
 
-            ImageView mImageView = view.findViewById(R.id.imageViewCard);
-            TextView mTextView = view.findViewById(R.id.textViewCard);
-            CardView card = view.findViewById(R.id.card);
+            ImageView mImageView = view.findViewById(R.id.imageViewCard); //get the ImageView
+            TextView mTextView = view.findViewById(R.id.textViewCard); //get the TextView
+            CardView card = view.findViewById(R.id.card); //get the CardView
 
             if(!isDataShuffled) {
                 shuffleData();
             }
             try {
+                //set the text of the TextView to the first element of the data array at position i
                 mTextView.setText(data[i][0]);
-                Picasso.with(getActivity().getApplicationContext()).load(data[i][1]).into(mImageView);
+                Picasso.with(getActivity().getApplicationContext()).load(data[i][1]).into(mImageView); //load the image using Picasso library
             } catch (Exception e){
-                Log.e("Homepage: ", ""+ e);
+                Log.e("Homepage: ", ""+ e); //log any errors that occur
             }
 
             card.setOnClickListener(view1 -> {
@@ -309,12 +347,8 @@ public class homepage extends Fragment {
                 Fragment currentFragment = fragmentManager.findFragmentById(R.id.homepage_container);
 
                 //disable interaction with homepage.
-                if (currentFragment instanceof food_card) {
-                    return; // do nothing if food_card fragment is already displayed
-                }
-
-                if (currentFragment instanceof CardWidgetFragment) {
-                    return; // do nothing if CardWidgetFragment fragment is already displayed
+                if (currentFragment instanceof food_card || currentFragment instanceof CardWidgetFragment) {
+                    return; // do nothing if food_card or CardWidgetFragment fragment is already displayed
                 }
 
                 //send data to the card
