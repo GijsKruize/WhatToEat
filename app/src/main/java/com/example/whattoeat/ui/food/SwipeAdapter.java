4217aad9d4a0de8bd995b248598687b85d6a27bb
Gijs Kruize <gbmkruize@gmail.com>
2023-04-19 15:16:34 +0200

    added contracts and comments

--- app/src/main/java/com/example/whattoeat/ui/food/SwipeAdapter.java
+++ app/src/main/java/com/example/whattoeat/ui/food/SwipeAdapter.java
@@ -25,16 +25,24 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder> 
     private List<String> name;
     private List<String> img;
 
-
-
-
+    /**
+     * Generator for the class SwipeAdapter, initializes all global varaiables
+     *
+     * @param context the current context
+     * @param IDs a list of strings
+     * @param name a list of strings
+     * @param img a list of strings
+     * @param styles a list of strings
+     */
     public SwipeAdapter(Context context, List<String> IDs, List<String> name, List<String> img, List<String> styles) {
         this.context = context;
+        // Randomize the inputted Lists with the same seed, so they stay coherent
         long seed = System.nanoTime();
         Collections.shuffle(img, new Random(seed));
         Collections.shuffle(name, new Random(seed));
         Collections.shuffle(IDs, new Random(seed));
         Collections.shuffle(styles, new Random(seed));
+        // Set given variables
         this.img = img;
         this.name = name;
         this.IDs = IDs;
@@ -49,18 +57,17 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull SwipeAdapter.ViewHolder holder, int position) {
-        holder.getTextView().setText(name.get(position));
-        Picasso.with(context).load("https://firebasestorage.googleapis.com/v0/b/what-to-eat-tue.appspot.com/o/the-best-caesar-salad-recipe-06-40e70f549ba2489db09355abd62f79a9.jpg?alt=media&token=220ba356-b1d6-4a21-ad0e-f305c19b46c2").into(holder.getImageView());
+        // No functionality added
     }
 
     @Override
     public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
+    // No functionality added
     }
 
     @Override
     public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
+        // No functionality added
     }
 
     @Override
@@ -78,18 +85,20 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder> 
         return i;
     }
 
+    // Give function to the getView function of the adapter
     @Override
     public View getView(int i, View view, ViewGroup parent) {
         ViewHolder holder;
         if (view == null) {
+            // If there is no view, generate a new item_koloda
             view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_koloda, parent, false);
-            holder = new ViewHolder(view);
-            view.setTag(holder);
+            holder = new ViewHolder(view); // Generate new holder
+            view.setTag(holder); // Bind ViewHolder to view
         } else {
             return null;
         }
-        Picasso.with(context).load(img.get(i)).into(holder.getImageView());
-        holder.getTextView().setText(name.get(i));
+        Picasso.with(context).load(img.get(i)).into(holder.getImageView()); // Load the i'th image into the image view
+        holder.getTextView().setText(name.get(i)); // Set the recipe name in the textbox
         return view;
     }
 
@@ -109,23 +118,24 @@ public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder> 
         return name.size();
     }
 
-
     class ViewHolder extends RecyclerView.ViewHolder {
         private final ShapeableImageView imageView;
         private final TextView textView;
 
+        // Generator for class ViewHolder
         public ViewHolder(View view) {
             super(view);
-            // Define click listener for the ViewHolder's View
-
+            // Get the image and TextView from the current view.
             imageView = (ShapeableImageView) view.findViewById(R.id.mealImage);
             textView = (TextView) view.findViewById(R.id.mealText);
         }
 
+        // Method that returns textView
         public TextView getTextView() {
             return textView;
         }
 
+        // Method that returns imageView
         public ShapeableImageView getImageView() {
             return imageView;
         }
--- app/src/main/java/com/example/whattoeat/ui/food/SwipeListener.java
+++ app/src/main/java/com/example/whattoeat/ui/food/SwipeListener.java
@@ -28,57 +28,78 @@ public class SwipeListener implements KolodaListener {
     private DatabaseReference generalRef;
     private DatabaseReference swipeRef;
     private String UID;
-
     private String mood;
 
-
+    /**
+     * Generator for the SwipeListener, initializes all global variables
+     *
+     * @param context current context
+     * @param IDs list of strings
+     * @param mood list of strings
+     * @param styles string
+     */
     public SwipeListener(Context context, List<String> IDs, List<String> styles, String mood) {
+        // Define global variables with parameters
         this.context = context;
         this.IDs = IDs;
         this.styles = styles;
+        this.mood = mood;
+        // Get instances of Firebase's Database and Authentication
         database = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        // Get authentication's user ID
         this.UID = mAuth.getUid();
+        // Set references for the 3 used tables in the database
         this.personalRef = FirebaseDatabase.getInstance().getReference().child("Personal Relation");
         this.swipeRef = FirebaseDatabase.getInstance().getReference().child("Swipe History");
-        this.generalRef =FirebaseDatabase.getInstance().getReference().child("General Relation");
-        this.mood = mood;
+        this.generalRef = FirebaseDatabase.getInstance().getReference().child("General Relation");
+
     }
 
     @Override
     public void onCardDoubleTap(int i) {
-
+        // No functionality added
     }
 
     @Override
     public void onCardDrag(int i, @NonNull View view, float v) {
-
+        // No functionality added
     }
 
     @Override
     public void onCardLongPress(int i) {
+        // If the user presses instead of swipes, display toast message
         Toast.makeText(context, "Please swipe", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onCardSingleTap(int i) {
+        // If the user taps instead of swipes, display toast message
         Toast.makeText(context, "Please swipe", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onCardSwipedLeft(int i) {
+        // Set the ID and style
         String mID = IDs.get(i + 1);
         String mStyle = styles.get(i + 1);
+        // Update the personal database and set recommendation for this mood false
         personalRef.child(UID).child(mood).child(mID).setValue(false);
+        // Update the swipehistory database to indicate user has swiped this card
         swipeRef.child(UID).child(mood).child(mID).setValue(true);
+
+        // Create transaction handler to handle decrementing the value in general reference
         generalRef.child(mood).child(mStyle).runTransaction(new Transaction.Handler() {
             @NonNull
             @Override
             public Transaction.Result doTransaction(@NonNull MutableData currentData) {
+                // Temporarily save the value of the rating
                 Long value = currentData.getValue(Long.class);
                 if (value == null) {
+                    // If current rating is 0, set it to -1
                     currentData.setValue(-1);
                 } else {
+                    // Else decrement the current value
                     currentData.setValue(value - 1);
                 }
                 return Transaction.success(currentData);
@@ -86,25 +107,33 @@ public class SwipeListener implements KolodaListener {
 
             @Override
             public void onComplete(@Nullable DatabaseError error, boolean committed, @Nullable DataSnapshot currentData) {
-
+                // do nothing
             }
         });
     }
 
     @Override
     public void onCardSwipedRight(int i) {
+        // Set the ID and style
         String mID = IDs.get(i + 1);
         String mStyle = styles.get(i + 1);
+        // Update the personal database and set recommendation for this mood true
         personalRef.child(UID).child(mood).child(mID).setValue(true);
+        // Update the swipehistory database to indicate user has swiped this card
         swipeRef.child(UID).child(mood).child(mID).setValue(true);
+
+        // Create transaction handler to handle decrementing the value in general reference
         generalRef.child(mood).child(mStyle).runTransaction(new Transaction.Handler() {
             @NonNull
             @Override
             public Transaction.Result doTransaction(@NonNull MutableData currentData) {
+                // Temporarily save the value of the rating
                 Long value = currentData.getValue(Long.class);
                 if (value == null) {
+                    // If current rating is 0, set it to 1
                     currentData.setValue(+1);
                 } else {
+                    // Else increment the current value
                     currentData.setValue(value + 1);
                 }
                 return Transaction.success(currentData);
@@ -112,29 +141,30 @@ public class SwipeListener implements KolodaListener {
 
             @Override
             public void onComplete(@Nullable DatabaseError error, boolean committed, @Nullable DataSnapshot currentData) {
-
+                // Do nothing
             }
         });
     }
 
     @Override
     public void onClickLeft(int i) {
-
+        // No functionality added
     }
 
     @Override
     public void onClickRight(int i) {
-
+        // No functionality added
     }
 
     @Override
     public void onEmptyDeck() {
+        // If all cards are swiped, display toast message
         Toast.makeText(context, "That was all to swipe for now!", Toast.LENGTH_SHORT).show();
-        ;
+
     }
 
     @Override
     public void onNewTopCard(int i) {
-
+        // No functionality added
     }
 }
